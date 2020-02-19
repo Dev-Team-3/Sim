e957c82f22882f370f79591f9a853483fdb47956
Simbongile Ndamase <61005958+SimDope2020@users.noreply.github.com>
2020-02-19 11:23:43 +0200

    Add files via upload

---
+++ water.sql
@@ -0,0 +1,6 @@
+barcode,price,size,quantity,product,id
+000070,R7,330,10,still,1
+000071,R11,330,15,sparkling,2
+000072,R10,330,19,flavoured,3
+000073,R7,330,20,still,4
+000074,R12,330,26,sparkling,5
