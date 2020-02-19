1f5f380d9aeda6072edd815e337217aaa39f7b4f
Simbongile Ndamase <61005958+SimDope2020@users.noreply.github.com>
2020-02-19 11:30:00 +0200

    Add files via upload

---
+++ cupcakes.sql
@@ -0,0 +1,11 @@
+barcode,price,size,quantity,product,id
+00000031,R8,Jumbo,20,Vanilla,1
+00000032,R8,Standard,25,Chocolate,2
+00000033,R10,Standard,15,Blueberry,3
+00000034,R12,Mini,10,"Red Velvet",4
+00000035,R13,Jumbo,20,Carrot,5
+00000036,R18,Jumbo,24,"Cheese Cake",6
+00000037,R16,Mini,12,Oats,7
+00000038,R22,Jumbo,19,"Apple ",8
+00000039,R23,Jumbo,15,Strawberry,9
+NULL,NULL,NULL,,NULL,10
