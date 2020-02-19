d1d97ad3e33430b5de069520606cfb46a38d701d
Simbongile Ndamase <61005958+SimDope2020@users.noreply.github.com>
2020-02-19 11:33:46 +0200

    Add files via upload

---
+++ chips.sql
@@ -0,0 +1,11 @@
+barcode,price,quantity,product,id
+00000050,R6,10,Simba,1
+00000051,R7,10,Lays,2
+00000052,R18,15,Pringles,3
+00000053,R8,15,Doritos,4
+00000054,R4,25,NilNaks,5
+00000055,R8,50,Fritos,6
+00000056,R12,55,Cheetos,7
+00000057,R13,60,Ruffles,8
+00000058,R14,75,"Willards Cheese Curls",9
+00000059,R14,40,"Big korn Bites",10
