43670db8d768edde012a658b61714883249dc7ac
Simbongile Ndamase <61005958+SimDope2020@users.noreply.github.com>
2020-02-19 11:27:34 +0200

    Add files via upload

---
+++ pies.sql
@@ -0,0 +1,8 @@
+barcode,price,size,quantity,product,id
+00000008,R15,Small,35,"Pepper Steak",1
+00000009,R15,Small,35,Chicken,2
+00000010,R20,Medium,25,Burger,3
+00000011,R18,Medium,30,"Plain Steak",4
+00000012,R15,Small,30,"Sausage Roll",5
+00000013,R22,Medim,20,"Salami & Cheese",6
+00000014,R29,Jumbo,26,"Steak & Kidney",7
