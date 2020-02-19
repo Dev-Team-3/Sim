c27db373649c2c7d67ccc8482ad54ec9c2712fa6
Simbongile Ndamase <61005958+SimDope2020@users.noreply.github.com>
2020-02-19 11:31:05 +0200

    Add files via upload

---
+++ cooldrinks.sql
@@ -0,0 +1,9 @@
+barcode,price,size,quantity,product,id
+00000039,R12,440,50,Coke,1
+00000040,R12,440ml,40,Fanta,2
+00000041,R7,500ml,25,Jive,3
+00000042,R20,500ml,10,Monster,4
+00000043,R12,330ml,50,Sprite,5
+00000044,R16,330ml,46,"Dr Pepper",6
+00000045,R12,330ml,50,Stoney,7
+00000045,R18,250ml,52,RedBull,8
